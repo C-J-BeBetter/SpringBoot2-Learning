36a73fecb1fabbebba243ca2419877dbd6e0cc42
chaijin <chaij@sunment.com>
2019-03-19 14:32:15 +0800

    第十九篇：轻松搞定重复提交（Redis实现分布式锁）

---
+++ chapter19/src/main/java/com/chaijin/controller/BookController.java
@@ -0,0 +1,27 @@
+package com.chaijin.controller;
+
+import com.chaijin.annotation.CacheLock;
+import com.chaijin.annotation.CacheParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * BookController
+ *
+ * @author Levin
+ * @since 2018/6/06 0031
+ */
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+
+    @CacheLock(prefix = "books")
+    @GetMapping
+    public String query(@CacheParam(name = "token") @RequestParam String token) {
+        return "success - " + token;
+    }
+
+}
